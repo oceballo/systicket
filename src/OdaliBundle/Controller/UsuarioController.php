8880b63c322028a791b0c1c636471bf09b0f6c0c
oceballo <odaliceballopimentel@gmail.com>
2017-12-15 10:19:01 -0400

    controladores de usuario POST y GET

--- src/OdaliBundle/Controller/UsuarioController.php
+++ src/OdaliBundle/Controller/UsuarioController.php
@@ -2,6 +2,9 @@
 
 namespace OdaliBundle\Controller;
 
+use DateTime;
+use OdaliBundle\Entity\user;
+use OdaliBundle\Form\userType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -14,6 +17,65 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
 class UsuarioController extends Controller
 {
+
+    /**
+     * @Route("/list", name="users_list")
+     */
+    public function listUsersAction(Request $request)
+    {
+        $users = $this->getDoctrine()->getRepository(user::class)->findAll();
+        return $this->render('OdaliBundle:Usuario:user_list.html.twig', array(
+            'users' => $users
+        ));
+    }
+
+
+    /**
+     * @Route("/", name="lista_usuario")
+     * @Method("GET")
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function indexAction(Request $request)
+    {
+        $users = $this->getDoctrine()->getRepository(user::class)->findAll();
+        $usersD = json_decode($this->get('serializer')->serialize($users, 'json'), true);
+
+        return new JsonResponse($usersD);
+    }
+
+
+
+    /**
+     * @Route("/{id}", name="get_usuario", requirements={"id"="\d+"} )
+     * @Method("GET")
+     * @param Request $request
+     * @param User $user
+     * @return JsonResponse
+     */
+    public function getUsuario(Request $request, user $user)
+    {
+        $userJ = json_decode($this->get('serializer')->serialize($user, 'json'), true);
+        return new JsonResponse($userJ);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="edit_usuario", requirements={"id"="\d+"} )
+     * @Method("GET")
+     * @param Request $request
+     * @param User $user
+     * @return JsonResponse
+     */
+    public function editUsuario(Request $request, user $user)
+    {
+        return $this->render('OdaliBundle:Usuario:user_edit.html.twig', array(
+            'user' => $user
+        ));
+    }
+
+
+
+
     /**
      * @Route("/",name="guardar_usuario", options={"expose"=true})
      * @Method({"POST"})
@@ -22,31 +84,33 @@ class UsuarioController extends Controller
     public function postUser(Request $request)
     {
 
-        dump($request);
-        die;
+        //dump($request);
+        //die;
 
         $data = json_decode($request->getContent(), true);
-        dump($data);
-        die;
+        //dump($data);
+        //die;
 
         $user = new user();
         $form = $this->createForm(userType::class, $user);
 
         $form->submit($data);
-        $user->setFecharegistro(new \DateTime());
+        $user->setFecharegistro(new DateTime());
         if ($form->isValid()) {
-            $em = $this->$this->getDoctrine()->getManager();
+            $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
 
             //dump("Es valido");
             //dump($user);
         } else {
-            //dump("No es valido");
+            dump("No es valido");
             //dump($this->>getFormErrors($form));
         }
 
-        $newUser = json_decode($this->get('serialize')->serialize($user, 'json'), true);
+        $newUser = json_decode($this->get('serializer')->serialize($user, 'json'), true);
         return new JsonResponse($newUser);
     }
+
+
 }
